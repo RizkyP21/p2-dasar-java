78b81dd556f58bda40c1611638c41f28b7fed5d8
DELL <rizkypertiwi2209@gmail.com>
2022-03-21 17:00:13 +0700

    Ekspresi

---
+++ src/Ekspresi.java
@@ -0,0 +1,12 @@
+public class Ekspresi {
+    public static void main(String[] args) {
+        int bilA = 5;
+        int bilB = bilA * 2;
+        int bilC = bilA + bilB;
+        System.out.println("Ini contoh ekspresi : bilA * 2="+bilB);
+        System.out.println("Ini contoh ekspresi : bilB + bilB ="+bilC);
+
+
+
+    }
+}
---
+++ src/Statement.java
@@ -0,0 +1,11 @@
+public class Statement {
+    public static void main(String[] args) {
+        System.out.println("Semoga saya bisa belajar");
+        System.out.println("Pemograman Berorientasi Objek");
+        int angka = 10;
+        char charA='A';
+        System.out.println(angka);
+        System.out.println(charA);
+    }
+
+}
